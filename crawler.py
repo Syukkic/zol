26154a677cf703fe850b539f3d8701b9547f6671
Yuki.N <Syukkic@gmail.com>
2021-11-04 22:12:42 +0800

    try to save data

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 from headers import headers
 from random import choice
 from lxml import etree
@@ -10,31 +11,40 @@ class Zol():
     def __init__(self) -> None:
 
         self.url = 'https://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_9_2_0_%i.html'
-
         self.header = {
             'User-Agent': str(choice(headers))
         }
     
     def fetch_html_page(self):
-        page = requests.get(self.url % 1, headers = self.header).text
-        return page
+        pages = []
+        for i in range(1, 51):
+            page = requests.get(self.url % i, headers = self.header).text
+            pages.append(page)
+        return pages
     
-    def parse(self, page):
+    def parse(self, pages):
 
-        html = etree.HTML(page)
-        phone_info = html.xpath('/html/body/div[5]/div[1]/div[4]/ul/li')
         result_list = []
+        for n in range(len(pages)):
 
-        for detail in phone_info:
-            detail_dict = {}
-            if detail.xpath('./h3/a/text()'):
-                detail_dict['name'] = list(filter(None, detail.xpath('./h3/a/text()')))
-            if detail.xpath('./div[1]/span[2]/b[2]/text()'):
-                detail_dict['price'] = list(filter(None, detail.xpath('./div[1]/span[2]/b[2]/text()')))
-            if detail.xpath('./div[2]/span[2]/text()'):
-                detail_dict['score'] = list(filter(None, detail.xpath('./div[2]/span[2]/text()')))
-            result_list.append(detail_dict)
-            
-        result_list = list(filter(None, result_list))
+            html = etree.HTML(pages[n])
+            phone_info = html.xpath('/html/body/div[5]/div[1]/div[4]/ul/li')
+
+            for detail in phone_info:
+                detail_dict = {}
+                if detail.xpath('./h3/a/text()'):
+                    detail_dict['name'] = list(filter(None, detail.xpath('./h3/a/text()')))[0].strip()
+                if detail.xpath('./div[1]/span[2]/b[2]/text()'):
+                    detail_dict['price'] = list(filter(None, detail.xpath('./div[1]/span[2]/b[2]/text()')))[0].strip()
+                if detail.xpath('./div[2]/span[2]/text()'):
+                    detail_dict['score'] = list(filter(None, detail.xpath('./div[2]/span[2]/text()')))[0].strip()
+                result_list.append(detail_dict)
+                
+            result_list = list(filter(None, result_list))
 
         return result_list
+
+
+    def storage(self, data):
+        df = pd.DataFrame(data)
+        print(df)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from crawler import Zol
 
 if __name__ == "__main__":
 
-    run = Zol()
-    page = run.fetch_html_page()
-    run.parse(page)
+    pages = Zol().fetch_html_page()
+    result = Zol().parse(pages)
+
+    Zol().storage(result)
